66a3f7cd293fcb0a5f94b8d0038a967952e33eb2
Zatherz <zatherz@linux.pl>
2018-02-03 05:21:02 +0100

    Add a Component+Version struct

---
+++ MTGInstaller/src/ComponentVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MTGInstaller.YAML;
+
+namespace MTGInstaller {
+	public struct ComponentVersion {
+		public ETGModVersion Version;
+		public ETGModComponent Component;
+
+		public ComponentVersion(ETGModComponent component, ETGModVersion version) {
+			Component = component;
+			Version = version;
+		}
+	}
+}
